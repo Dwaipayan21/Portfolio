c4022c102d5f98c54db24e59781705d92b419567
dwaipayan <dwaipayanbarui99209@gmail.com>
2026-08-26 08:52:02 +0530

    added welcome animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Welcome from './components/Welcome'
 
 const App = () => {
   return (
-    <div>
       <main>
         <Navbar />
+        <Welcome />
       </main>
-    </div>
   )
 }
 
