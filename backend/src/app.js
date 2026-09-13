b4395f636393bd0b33034bfd30c318ec27c5bbb5
dwaipayan <dwaipayanbarui99209@gmail.com>
2026-09-13 10:44:09 +0530

    fix the app.js file

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,8 +27,7 @@ app.use("/api/resume", resumeRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  app.get(/^(?!\/api(?:\/|$)).*/, (req, res) => {    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
   });
 }
 
